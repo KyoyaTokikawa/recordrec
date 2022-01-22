a637d9a3f9597645edaa710ccbc45b53e69fc3cb
pegurin <pegurin.t.k@gmail.com>
2022-01-22 11:03:00 +0900

    add Wave and Quote source from demo site to Home

---
+++ src/components/WaveButton.vue
@@ -0,0 +1,103 @@
+<template>
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+
+<a href="javascript:void(0)">
+  <span>Click</span>
+  <div class="wave"></div>
+</a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Righteous', cursive;
+  }
+
+  body {
+    min-height: 100vh;
+    background-color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center
+  }
+
+  a {
+    width: 100px;
+    padding: 10px 20px;
+    position: relative;
+    display: block;
+    text-decoration: none;
+    overflow: hidden;
+    
+    &:hover {
+      .wave {
+        top: -120px;
+      }
+    }
+
+    span {
+      position: relative;
+      z-index: 1;
+      color: white;
+      font-size: 12px;
+      letter-spacing: 8px;
+    }
+
+    .wave {
+      width: 200px;
+      height: 200px;
+      background-color: #444857;
+      box-shadow: inset 0 0 50px rgba(0,0,0,.5);
+      position: absolute;
+      left: 0;
+      top: -80px;
+      transition: 0.4s;
+
+      &::before, &::after {
+        width: 200%;
+        height: 200%;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 25%;
+        transform: translate(-40%,-80%);
+      }
+
+      &::before {
+        border-radius: 45%;
+        background-color: rgba(20,20,20,1);
+        animation: wave 5s linear infinite;
+      }
+
+      &::after {
+        border-radius: 40%;
+        background-color: rgba(20,20,20,0.5);
+        animation: wave 10s linear infinite;
+      }
+    }
+  }
+
+  @keyframes wave {
+    0% {
+      transform: translate(-50%,-75%) rotate(0deg);
+    }
+
+    100% {
+      transform: translate(-50%,-75%) rotate(360deg);
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,61 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
+		<div class="col-12 md:col-6">
+			<div class="card p-fluid">
+				<h5>Vertical</h5>
+				<div class="field">
+					<label for="name1">Name</label>
+					<InputText id="name1" type="text" />
+				</div>
+				<div class="field">
+					<label for="email1">Email</label>
+					<InputText id="email1" type="text" />
+				</div>
+				<div class="field">
+					<label for="age1">Age</label>
+					<InputText id="age1" type="text" />
+				</div>
+			</div>
+
+			<div class="card p-fluid">
+				<h5>Vertical Grid</h5>
+				<div class="formgrid grid">
+					<div class="field col">
+						<label for="name2">Name</label>
+						<InputText id="name2" type="text" />
+					</div>
+					<div class="field col">
+						<label for="email2">Email</label>
+						<InputText id="email2" type="text" />
+					</div>
+					<div class="field col">
+						<label for="email2">Email</label>
+						<InputText id="email2" type="text" />
+					</div>
+				</div>
+				<div class="formgrid grid">
+					<div class="field col">
+						<label for="name2">Name</label>
+						<InputText id="name2" type="text" />
+					</div>
+					<div class="field col">
+						<label for="email2">Email</label>
+						<InputText id="email2" type="text" />
+					</div>
+					<div class="field col">
+						<label for="email2">Email</label>
+						<InputText id="email2" type="text" />
+					</div>
+				</div>
+
+			</div>
+		</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/WaveButton.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
