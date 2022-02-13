d678204bbab16906f6e0a9b3759497a17efe5e37
pegurin <pegurin.t.k@gmail.com>
2022-02-13 21:05:29 +0900

    server追加

---
+++ server/dist/config/App.config.js
@@ -0,0 +1,16 @@
+export default {
+    server: 'DESKTOP-JELOOV5',
+    authentication: {
+        type: "default",
+        options: {
+            userName: 'sa',
+            password: 'pegunike39',
+        }
+    },
+    options: {
+        encrypt: false,
+        // instanceName: 'MSSQLSERVER',
+        database: "RecordRec"
+    }
+};
+//# sourceMappingURL=App.config.js.map
---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import router from "./router/router.js";
+import bodyparser from "body-parser"
+import path from 'path'
+
+const app = express()
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+const dirname = path.dirname(new URL(import.meta.url).pathname)
+
+app.use(express.static(path.join(dirname, "public")))
+app.use(router);
+
+export default app.listen(3000, () => {
+    console.log("App is running at http://localhost:3000")
+});
