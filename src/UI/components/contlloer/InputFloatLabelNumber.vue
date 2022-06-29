c9288e0612e6bc4f10d0ab39ec9fdfb7604ad6aa
K-tokikawa <tokikawa@alte.co.jp>
2022-06-29 10:53:05 +0900

    InputFloatLabelNumberの実装 UserMasterの改修

---
+++ src/UI/components/contlloer/InputFloatLabelNumber.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="field">
+        <span class="p-float-label">
+            <InputNumber ref="componentRef" v-model="value" :min="MinValue" :max="MaxValue" :show-buttons="ShowButton" :useGrouping="Grouping" @focus="FocusEvent" />
+            <label for="labelName">{{Label_Name}}</label>
+        </span>                        
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+export default defineComponent({
+    name:'InputFloatLabelNumber',
+    props:{
+        Label_Name: String,
+        MinValue: Number,
+        MaxValue: Number,
+        ShowButton: Boolean,
+        Grouping: Boolean,
+    },
+    emits:["FocusEvent"],
+    setup(props, context){
+
+        const state = reactive({
+            number: null as number | null
+        })
+        const value = ref<number | null>(null);
+        const componentRef = ref<HTMLElement>()
+
+        const Focus = () => {
+            componentRef.value?.focus()
+        }
+        const FocusEvent = ()=> {
+            context.emit("FocusEvent")
+        }
+        const Value = () => {
+            return value
+        }
+        return {
+            value,
+            componentRef,
+            state,
+            Focus,
+            FocusEvent,
+            Value
+        };
+    }
+})
+
+</script>
+<style lang="scss">
+.field {
+    margin: 0px;
+}
+</style>
--- src/UI/views/UserMaster.vue
+++ src/UI/views/UserMaster.vue
@@ -15,9 +15,9 @@
                 </div>
                 <h6 style="text-align: left;">BirthDay</h6>
                 <div class="field col formgroup-inline" style="width: 100%;">
-                    <InputFloatLabelText :Label_Name="Year"/>
-                    <InputFloatLabelText :Label_Name="Month"/>
-                    <InputFloatLabelText :Label_Name="Day"/>
+                    <InputFloatLabelNumber ref="InputYear"  :Label_Name="Year"  :MaxValue="MaxYear"  :MinValue="MinYear"  :Grouping="Grouping" />
+                    <InputFloatLabelNumber ref="InputMonth" :Label_Name="Month" :MaxValue="MaxMonth" :MinValue="MinMonth" :Grouping="Grouping" />
+                    <InputFloatLabelNumber ref="InputDay"   :Label_Name="Day"   :MaxValue="MaxDay"   :MinValue="MinDay"   :Grouping="Grouping" @FocusEvent="dayfocus"/>
                 </div>
             </div>
         </div>
@@ -25,21 +25,43 @@
 </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, Ref } from "vue";
+import InputFloatLabelNumber from "../components/contlloer/InputFloatLabelNumber.vue";
 import InputFloatLabelText from "../components/contlloer/InputFloatLabelText.vue";
-
 export default defineComponent({
     components:{
-        InputFloatLabelText
+        InputFloatLabelText,
+        InputFloatLabelNumber
     },
     setup(){
-        const UserID ='UserID'
-        const FirstName = 'FirstName'
-        const FamilyName = 'FamilyName'
-        const MiddleName = 'MiddleName'
-        const Year = 'Year'
-        const Month = 'Month'
-        const Day = 'Day'
+        const UserID     ='UserID';
+        const FirstName  = 'FirstName';
+        const FamilyName = 'FamilyName';
+        const MiddleName = 'MiddleName';
+        const Year       = 'Year';
+        const MaxYear    = 9999;
+        const MinYear    = 1990;
+        const Month      = 'Month';
+        const MaxMonth   = 12;
+        const MinMonth   = 1;
+        const Day        = 'Day';
+        const MaxDay     = ref(31);
+        const MinDay     = ref(1);
+        const Grouping = false;
+        const YeraEvent = 'InputYearMonth'
+        const InputYear = ref<InstanceType<typeof InputFloatLabelNumber>>()
+        const InputMonth = ref<InstanceType<typeof InputFloatLabelNumber>>()
+        const InputDay = ref<InstanceType<typeof InputFloatLabelNumber>>()
+        const InputEventYear = ((value: Ref<number>) => {
+            InputYear.value?.value
+            console.log(value)
+        })
+
+        const dayfocus = () => {
+            console.log(InputYear.value?.value)
+            console.log(InputMonth.value?.value)
+        }
+
         return {
             UserID,
             FirstName,
@@ -48,6 +70,19 @@ export default defineComponent({
             Year,
             Month,
             Day,
+            MaxYear,
+            MinYear,
+            MaxMonth,
+            MinMonth,
+            MaxDay,
+            MinDay,
+            Grouping,
+            YeraEvent,
+            InputYear,
+            InputMonth,
+            InputDay,
+            InputEventYear,
+            dayfocus
         }
     }
 })
